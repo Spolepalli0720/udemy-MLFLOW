87345d4eb8246bf08598eddc0a760381fe9f8eca
Spolepalli0720 <68381333+Spolepalli0720@users.noreply.github.com>
2026-01-18 06:54:26 +0530

    cleanup

--- main.py
+++ main.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
+        print("  R2: %s" % r2)
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
         mlflow.log_metric("rmse", rmse)
